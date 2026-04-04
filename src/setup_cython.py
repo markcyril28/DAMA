e4630e70b1a7570a32b9b8a120d7b9b90b9f4464
markcyril28 <mercadomarkcyril28@gmail.com>
2026-04-04 21:12:43 +0800

    Add setup script for building Cython extensions to enhance performance of encoding functions

---
+++ src/setup_cython.py
@@ -0,0 +1,26 @@
+"""Build Cython extensions for performance-critical encoding functions."""
+
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy as np
+
+extensions = [
+    Extension(
+        "dama.ai.ml._fast_encode",
+        sources=["dama/ai/ml/_fast_encode.pyx"],
+        include_dirs=[np.get_include()],
+        define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+    ),
+]
+
+setup(
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={
+            "boundscheck": False,
+            "wraparound": False,
+            "cdivision": True,
+            "language_level": "3",
+        },
+    ),
+)
